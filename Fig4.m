5504ad96dfbaffaa8c0577ebc34d7ab5d205851b
Lemon-xah <limenghan21@mails.ucas.ac.cn>
2025-10-21 11:07:37 +0800

    Fig4.m

--- Fig4.m
+++ Fig4.m
@@ -22,8 +22,8 @@ for i = 1:length(L)
             h = sqrt(sigma2).*(randn(L(i),SampNum) + 1i*randn(L(i),SampNum));  
             eta = abs(h).^2; 
             [K, N, r(i,j), K_fec, N_fec, r_fec(i,j)] = fun_opt_dk2(L(i), p_b, eta(:,j), N_total, zeta);
-            [k_int,N_int,r_int(i,j)] = myRounding(K,N,L(i),p_b,eta(:,j),N_total,zeta);  
-            [k_fec_int,N_fec_int,r_fec_int(i,j)] = myRounding(K_fec,N_fec,L(i),p_b,eta(:,j),N_total,zeta);
+            [k_int,N_int,r_int(i,j)] = finalRounding(K,N,L(i),p_b,eta(:,j),N_total,zeta);  
+            [k_fec_int,N_fec_int,r_fec_int(i,j)] = finalfecRounding(K_fec,N_fec,L(i),p_b,eta(:,j),N_total,zeta);
             r_final(i,j)= obj0(k_int',N_int',eta(:,j),p_b);
             r_fec_final(i,j)= fec0(k_fec_int',N_fec_int',eta(:,j));
         catch ME
@@ -39,3 +39,4 @@ mean_r = mean(filtered_r, 2);
 negative_cols_fec = any(r_fec_final < 0 | isnan(r_fec_final), 1); 
 filtered_r_fec = r_fec_final(:, ~negative_cols_fec); 
 mean_r_fec = mean(filtered_r_fec, 2); 
+
